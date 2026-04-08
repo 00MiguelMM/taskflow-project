ca5cdb0fc638ea7c5ecf1aab7b0826041f664fdc
00MiguelMM <miguelmn637@gmail.com>
2026-04-08 17:32:28 +0200

    git commit -m "fix: adaptar backend de Vercel con función serverless"

---
+++ Server/api/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import taskRoutes from "../src/routes/task.routes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1/tasks", taskRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando 🚀");
+});
+
+export default app;
